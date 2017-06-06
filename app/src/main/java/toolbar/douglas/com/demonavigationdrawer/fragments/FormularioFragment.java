a188106c8a8b64ab64fb6f5e4836b6e4e0303762
douglas <alaraujo.douglas@gmail.com>
2017-06-05 22:58:57 -0300

    Commit fragments funcionando

--- app/src/main/java/toolbar/douglas/com/demonavigationdrawer/fragments/FormularioFragment.java
+++ app/src/main/java/toolbar/douglas/com/demonavigationdrawer/fragments/FormularioFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,12 +15,12 @@ import toolbar.douglas.com.demonavigationdrawer.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FormularioFragment extends Fragment {
+public class FormularioFragment extends Fragment implements View.OnClickListener {
     private EditText etValor1;
     private EditText etValor2;
     private TextView tvResultado;
     private Integer resultado;
-
+    private Button btSomar,btSubtrair,btMultiplicar,btDividir;
     public FormularioFragment() {
         // Required empty public constructor
     }
@@ -34,15 +35,25 @@ public class FormularioFragment extends Fragment {
         etValor1 = (EditText) itemView.findViewById(R.id.etValor1);
         etValor2 = (EditText) itemView.findViewById(R.id.etValor2);
         tvResultado = (TextView) itemView.findViewById(R.id.tvResultado);
+        btDividir = (Button) itemView.findViewById(R.id.btnDivide);
+        btSomar = (Button) itemView.findViewById(R.id.btnSoma);
+        btMultiplicar = (Button) itemView.findViewById(R.id.btnMultiplica);
+        btSubtrair = (Button) itemView.findViewById(R.id.btnSubtrae);
+        btDividir.setOnClickListener(this);
+        btSomar.setOnClickListener(this);
+        btMultiplicar.setOnClickListener(this);
+        btSubtrair.setOnClickListener(this);
+
         return itemView;
     }
 
     public void somar(View view) {
         Integer valor1;
         Integer valor2;
-        valor1 = Integer.parseInt(etValor1.getText().toString());
+        valor1 = Integer.parseInt(etValor1.getEditableText().toString());
         valor2 = Integer.parseInt(etValor2.getText().toString());
         resultado = valor1 + valor2;
+        setResultado(resultado);
     }
 
     public void subtrair(View view) {
@@ -51,6 +62,7 @@ public class FormularioFragment extends Fragment {
         valor1 = Integer.parseInt(etValor1.getText().toString());
         valor2 = Integer.parseInt(etValor2.getText().toString());
         resultado = valor1 - valor2;
+        setResultado(resultado);
     }
 
     public void multiplicar(View view) {
@@ -59,6 +71,7 @@ public class FormularioFragment extends Fragment {
         valor1 = Integer.parseInt(etValor1.getText().toString());
         valor2 = Integer.parseInt(etValor2.getText().toString());
         resultado = valor1 * valor2;
+        setResultado(resultado);
     }
 
     public void dividir(View view) {
@@ -67,9 +80,29 @@ public class FormularioFragment extends Fragment {
         valor1 = Integer.parseInt(etValor1.getText().toString());
         valor2 = Integer.parseInt(etValor2.getText().toString());
         resultado = valor1 + valor2;
+        setResultado(resultado);
     }
 
     private void setResultado(Integer resultado) {
-        tvResultado.setText(resultado);
+        tvResultado.setText(String.valueOf(resultado));
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnDivide:
+                dividir(v);
+                break;
+            case R.id.btnMultiplica:
+                multiplicar(v);
+                break;
+            case R.id.btnSoma:
+                somar(v);
+                break;
+            case R.id.btnSubtrae:
+                subtrair(v);
+                break;
+        }
+
     }
 }
